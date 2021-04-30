4f14d8c1b25f7054a3842f725e85a3cd835a893b
Lucas Calvet Santos <up201904517@edu.fe.up.pt>
2021-04-30 02:10:01 +0100

    Added error handling and other stuff

--- client.c
+++ client.c
@@ -9,22 +9,17 @@
 #include <string.h>
 #include <errno.h>
 #include <pthread.h>
-#include "common.h"
-#include <math.h>
+#include "./common.h"
 
 #define OK 0
+#define PF_MAX_CHARS 40
 
 enum Operation
 {
     IWANT,
-    RECVD,
-    TSKEX,
-    TSKDN,
     GOTRS,
-    TLATE,
     CLOSD,
-    GAVUP,
-    FAILD
+    GAVUP
 };
 
 pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
@@ -35,162 +30,199 @@ bool server_closed;
 
 void print_usage()
 {
-    printf("\n"
-           "Usage: c <-t nsecs> <fifoname>\n"
-           "\n"
-           "nsecs: nº (aproximado) de segundos que o programa deve funcionar\n" //ja se traduz
-           "fifoname: - nome (absoluto ou relativo) do canal público de comunicação com nome (FIFO) por onde o Cliente envia pedidos ao Servidor\n");
-    exit(EXIT_SUCCESS);
+    printf("Usage: c <-t nsecs> <fifoname>\n"
+           "nsecs - the number of seconds the program shall run for (approximately)\n"
+           "fifoname - the name of the public comunication channel (FIFO) used by the client to send requests to the server\n\n");
 }
 
 void operation_register(enum Operation op, Message msg)
 {
     char op_str[6];
+    int print_result = 0;
     switch (op)
     {
     case IWANT:
-        strcpy(op_str, "IWANT");
-        break;
-    case RECVD:
-        strcpy(op_str, "RECVD");
-        break;
-    case TSKEX:
-        strcpy(op_str, "TSKEX");
-        break;
-    case TSKDN:
-        strcpy(op_str, "TSKDN");
+        print_result = snprintf(op_str, sizeof(op_str), "IWANT");
         break;
     case GOTRS:
-        strcpy(op_str, "GOTRS");
-        break;
-    case TLATE:
-        strcpy(op_str, "2LATE");
+        print_result = snprintf(op_str, sizeof(op_str), "GOTRS");
         break;
     case CLOSD:
-        strcpy(op_str, "CLOSD");
+        print_result = snprintf(op_str, sizeof(op_str), "CLOSD");
         break;
     case GAVUP:
-        strcpy(op_str, "GAVUP");
-        break;
-    case FAILD:
-        strcpy(op_str, "FAILD");
+        print_result = snprintf(op_str, sizeof(op_str), "GAVUP");
         break;
     default:
         break;
     }
 
-    printf("%ld ; %d ; %d ; %d ; %lu ; %d ; %s\n", time(NULL), msg.rid, msg.tskload, msg.pid, msg.tid, msg.tskres, op_str);
+    if (print_result < 0)
+    {
+        error(EXIT_FAILURE, ENOTTY, "snprintf failed");
+    }
+
+    if (printf("%ld ; %d ; %d ; %d ; %lu ; %d ; %s\n", time(NULL), msg.rid, msg.tskload, msg.pid, msg.tid, msg.tskres, op_str) < 0)
+    {
+        error(EXIT_FAILURE, ENOTTY, "printf failed");
+    }
 }
 
 void *make_request(void *arg)
 {
     Message msg;
     msg.rid = *(int *)arg;
+    free((int *)arg);
     int pnp;
     pthread_t tid = pthread_self();
     pid_t pid = getpid();
     msg.tid = tid;
     msg.pid = pid;
-    msg.tskload = 1 + (rand() % 9);
+    unsigned seed = tid;
+    msg.tskload = 1 + (rand_r(&seed) % 9);
     msg.tskres = -1;
 
     pthread_mutex_lock(&mut);
-    if (write(np, &msg, sizeof(Message)) != OK && errno == EAGAIN){
-        server_closed = true;
+    if (write(np, &msg, sizeof(Message)) == -1)
+    {
+        if (errno == EAGAIN)
+            server_closed = true;
+        else
+        {
+            perror("cannot write to public fifo");
+            pthread_mutex_unlock(&mut);
+            return NULL;
+        }
     }
     pthread_mutex_unlock(&mut);
-    
-    if(server_closed){
+
+    if (server_closed)
+    {
         return NULL;
     }
 
     operation_register(IWANT, msg);
 
-    //char *private_fifo_name = (char *)malloc((7 + floor(log10((double) msg.pid)) + 1 + floor(log10((double) msg.tid)) + 1) * sizeof(char));
-    char *private_fifo_name = (char *)malloc(40 * sizeof(char));
-    sprintf(private_fifo_name, "/tmp/%d.%lu", msg.pid, msg.tid);
-    mkfifo(private_fifo_name, 0666);
-
-    int read_no = -1;
-    //read(pnp, &msg, sizeof(Message));
-
-    while ((pnp = open(private_fifo_name, O_RDONLY | O_NONBLOCK)) < 0 && (time(NULL) - start_time) < nsecs)
-    ;
-
-    while (read_no <= 0 && (time(NULL) - start_time) < nsecs)
+    char private_fifo_name[PF_MAX_CHARS];
+    if (snprintf(private_fifo_name, sizeof(private_fifo_name), "/tmp/%d.%lu", msg.pid, msg.tid) < 0)
     {
-        read_no = read(pnp, &msg, sizeof(Message));
+        perror("snprintf failed");
+        return NULL;
+    }
+    if (mkfifo(private_fifo_name, 0666) != OK)
+    {
+        perror("cannot create private fifo");
+        return NULL;
     }
 
-    if (read_no == -1 || read_no == 0)
+    while ((pnp = open(private_fifo_name, O_RDONLY | O_NONBLOCK)) < 0 && (time(NULL) - start_time) <= nsecs)
+    {
+        if (errno != EWOULDBLOCK)
+        {
+            unlink(private_fifo_name);
+            perror("cannot open private fifo");
+            return NULL;
+        }
+        if (server_closed)
+        {
+            unlink(private_fifo_name);
+            return NULL;
+        }
+    }
+
+    int read_no = -1;
+    while ((read_no = read(pnp, &msg, sizeof(Message))) <= 0 && (time(NULL) - start_time) <= nsecs)
+    {
+        if (server_closed || (read_no == -1 && errno != EAGAIN))
+        {
+            close(pnp);
+            unlink(private_fifo_name);
+            perror("cannot read private fifo");
+            return NULL;
+        }
+    }
+
+    if (!server_closed && ((read_no == 0 || read_no == -1) && (time(NULL) - start_time) > nsecs))
     {
         operation_register(GAVUP, msg);
         close(pnp);
         unlink(private_fifo_name);
-        free(private_fifo_name);
         return NULL;
     }
 
     msg.pid = pid;
     msg.tid = tid;
 
-    if (msg.tskres == -1)
-    {
-        operation_register(CLOSD, msg);
+    if (!server_closed){
+        if (msg.tskres == -1)
+        {
+            operation_register(CLOSD, msg);
+        }
+        else
+        {
+            operation_register(GOTRS, msg);
+        }
     }
-    else
-    {
-        operation_register(GOTRS, msg);
-    }
+    
     close(pnp);
     unlink(private_fifo_name);
-    free(private_fifo_name);
     return NULL;
 }
 
 int main(int argc, char *argv[], char *envp[])
-{ //argc inclui nome do programa vai ter sempre de ser 4
+{
     start_time = time(NULL);
     if (argc != 4)
     {
+        print_usage();
         error(EXIT_FAILURE, EINVAL, "incorrect number of arguments");
     }
     if (strcmp(argv[1], "-t"))
     {
+        print_usage();
         error(EXIT_FAILURE, EINVAL, "incorrect argument");
     }
 
     nsecs = atoi(argv[2]);
     if (nsecs <= 0)
     {
+        print_usage();
         error(EXIT_FAILURE, EINVAL, "invalid time");
     }
 
     char *fifo_name = argv[3];
 
-    while ((np = open(fifo_name, O_WRONLY | O_NONBLOCK)) < 0 && (time(NULL) - start_time) < nsecs)
-        ;
+    while ((np = open(fifo_name, O_WRONLY | O_NONBLOCK)) < 0 && (time(NULL) - start_time) <= nsecs)
+    {
+        if (errno != ENOENT && errno != ENXIO && errno != EWOULDBLOCK)
+        {
+            error(EXIT_FAILURE, errno, "cannot open public fifo");
+        }
+    }
 
     server_closed = false;
 
     pthread_t tid;
     int current_rid = 0;
     int errn;
-    //int arg[2];
     int *arg;
+    unsigned seed = time(NULL);
 
-    while ((time(NULL) - start_time) < nsecs && !server_closed)
+    while ((time(NULL) - start_time) <= nsecs && !server_closed)
     {
         arg = (int *)malloc(sizeof(int));
+        if (arg == NULL)
+        {
+            error(EXIT_FAILURE, errno, "cannot allocate memory");
+        }
         *arg = current_rid++;
         if ((errn = pthread_create(&tid, NULL, make_request, arg)) != OK)
         {
             error(EXIT_FAILURE, errn, "cannot create a new thread");
         }
-        usleep(1000 * (rand() % 50));
+        usleep(1000 * (rand_r(&seed) % 50));
     }
-
     sleep(1);
     pthread_mutex_destroy(&mut);
-    printf("Elapsed time: %ld s\n", time(NULL) - start_time);
+    close(np);
 }
